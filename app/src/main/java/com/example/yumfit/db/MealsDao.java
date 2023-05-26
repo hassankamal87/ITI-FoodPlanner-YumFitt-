f4eb14a94cce7a6dbfde132cf001954248aba401
Hassan kamal <118057412+hassankamal87@users.noreply.github.com>
2023-05-27 00:21:44 +0300

    RoomDatabase Class Dao Entity

---
+++ app/src/main/java/com/example/yumfit/db/MealsDao.java
@@ -0,0 +1,23 @@
+package com.example.yumfit.db;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import com.example.yumfit.pojo.Meal;
+
+import java.util.List;
+
+@Dao
+public interface MealsDao {
+    @Insert
+    public void insertMeal(Meal meal);
+
+    @Delete
+    public void deleteMeal(Meal meal);
+
+    @Query("SELECT * FROM meals_table")
+    LiveData<List<Meal>> getAllMeals();
+}
---
+++ app/src/main/java/com/example/yumfit/db/MealsDatabase.java
@@ -0,0 +1,25 @@
+package com.example.yumfit.db;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.example.yumfit.pojo.Meal;
+
+@Database(entities = Meal.class, version = 1)
+public abstract class MealsDatabase extends RoomDatabase {
+    private static MealsDatabase instance;
+    public abstract MealsDao getMealsDao();
+
+    public static synchronized MealsDatabase getInstance(Context context){
+        if(instance == null){
+            instance = Room.databaseBuilder(context.getApplicationContext(), MealsDatabase.class, "Meals_database")
+                    .fallbackToDestructiveMigration()
+                    .allowMainThreadQueries()
+                    .build();
+        }
+        return instance;
+    }
+}
